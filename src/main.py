686c9d6dca1df1dbed7c943b261ef174cc8f4449
mHaines331 <mhaines9219@gmail.com>
2023-12-28 22:01:32 -0800

    basic UI

--- src/main.py
+++ src/main.py
@@ -3,22 +3,24 @@ from bs4 import BeautifulSoup
 import re
 import requests
 
-url = "https://en.wikipedia.org/wiki/French_battleship_Bouvet"
+url = "https://en.wikipedia.org/wiki/2023_Jishishan_earthquake"
 response = requests.get(url)
 
 the_array = []
+if response.status_code==200:
+    soup = BeautifulSoup(response.text, 'html.parser')
 
-soup = BeautifulSoup(response.text, 'html.parser')
+    def contains_the(text):
+        return re.findall(r'\bthe\b',text, re.IGNORECASE)
 
-def contains_the(text):
-    return re.findall(r'\bthe\b',text, re.IGNORECASE)
+    the_elements = soup.find_all(string=contains_the)
 
-the_elements = soup.find_all(string=contains_the)
+    for element in the_elements:
+        matches = contains_the(element)
+        for match in matches:
+            the_array.append(match)
 
-for element in the_elements:
-    matches = contains_the(element)
-    for match in matches:
-        the_array.append(match)
-
-    length = len(the_array)
-print(length)
+        length = len(the_array)
+    print(length)
+else:
+    print("Bummer man")
